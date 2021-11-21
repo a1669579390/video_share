528b0c629a0873e60ead0002d793aea33625e322
yangtongmiao <1669579390@qq.com>
2021-11-21 20:59:50 +0800

    选集列表渲染

--- lib/player/player.dart
+++ lib/player/player.dart
@@ -1,5 +1,7 @@
 import 'dart:collection';
+import 'dart:developer';
 import 'dart:io';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
@@ -44,6 +46,7 @@ class _InAppWebViewScreenState extends State<InAppWebViewScreen>
   List<SplitList?> _data = [];
   List<Widget> _tabs = [];
   List<Widget> _tabViews = [];
+  RxInt _viewsHeight = 0.obs;
 
   late TabController _tabcontroller;
 
@@ -55,7 +58,7 @@ class _InAppWebViewScreenState extends State<InAppWebViewScreen>
     return Get.arguments['cid'];
   }
 
-  getTabs() {
+  getTabs() async {
     List<Widget> tabs = [];
     List<Widget> tabViews = [];
     _data.forEach((element) {
@@ -67,61 +70,11 @@ class _InAppWebViewScreenState extends State<InAppWebViewScreen>
           onPressed: () {},
         ));
       }
-      tabViews.add(ConstrainedBox(
-          constraints: BoxConstraints(
-            minWidth: double.infinity, //宽度尽可能大
-            minHeight: 150.0, //最小高度为50像素
-            maxHeight: double.infinity,
-          ),
-          child: Wrap(
-              spacing: 12.0, // 主轴(水平)方向间距
-              runSpacing: 4.0, // 纵轴（垂直）方向间距
-              alignment: WrapAlignment.start, //沿主轴方向居中
-              children: views)));
-
-      // tabViews.add(Wrap(
-      //   spacing: 8.0, // 主轴(水平)方向间距
-      //   runSpacing: 4.0, // 纵轴（垂直）方向间距
-      //   alignment: WrapAlignment.start, //沿主轴方向居中
-      //   children: [
-      //     TextButton(
-      //       child: Text('11'),
-      //       onPressed: () {},
-      //     ),
-      //     TextButton(
-      //       child: Text('11'),
-      //       onPressed: () {},
-      //     ),
-      //     TextButton(
-      //       child: Text('11'),
-      //       onPressed: () {},
-      //     ),
-      //     TextButton(
-      //       child: Text('11'),
-      //       onPressed: () {},
-      //     ),
-      //     TextButton(
-      //       child: Text('11'),
-      //       onPressed: () {},
-      //     ),
-      //     TextButton(
-      //       child: Text('11'),
-      //       onPressed: () {},
-      //     ),
-      //     TextButton(
-      //       child: Text('11'),
-      //       onPressed: () {},
-      //     ),
-      //     TextButton(
-      //       child: Text('11'),
-      //       onPressed: () {},
-      //     ),
-      //     TextButton(
-      //       child: Text('11'),
-      //       onPressed: () {},
-      //     )
-      //   ],
-      // ));
+      tabViews.add(Wrap(
+          spacing: 12.0, // 主轴(水平)方向间距
+          runSpacing: 4.0, // 纵轴（垂直）方向间距
+          alignment: WrapAlignment.start, //沿主轴方向居中
+          children: views));
     });
     setState(() {
       _tabs = tabs;
@@ -132,40 +85,38 @@ class _InAppWebViewScreenState extends State<InAppWebViewScreen>
   @override
   void initState() {
     super.initState();
-    _loading();
-    // getTabs();
-    print("3");
+
     _tabcontroller = TabController(length: 0, vsync: this);
-    contextMenu = ContextMenu(
-        menuItems: [
-          ContextMenuItem(
-              androidId: 1,
-              iosId: "1",
-              title: "Special",
-              action: () async {
-                print("Menu item Special clicked!");
-                // print(await webViewController?.getSelectedText());
-                await webViewController?.clearFocus();
-              })
-        ],
-        options: ContextMenuOptions(hideDefaultSystemContextMenuItems: false),
-        onCreateContextMenu: (hitTestResult) async {
-          // print("onCreateContextMenu");
-          // print(hitTestResult.extra);
-          // print(await webViewController?.getSelectedText());
-        },
-        onHideContextMenu: () {
-          print("onHideContextMenu");
-        },
-        onContextMenuActionItemClicked: (contextMenuItemClicked) async {
-          var id = (Platform.isAndroid)
-              ? contextMenuItemClicked.androidId
-              : contextMenuItemClicked.iosId;
-          print("onContextMenuActionItemClicked: " +
-              id.toString() +
-              " " +
-              contextMenuItemClicked.title);
-        });
+    // contextMenu = ContextMenu(
+    //     menuItems: [
+    //       ContextMenuItem(
+    //           androidId: 1,
+    //           iosId: "1",
+    //           title: "Special",
+    //           action: () async {
+    //             print("Menu item Special clicked!");
+    //             // print(await webViewController?.getSelectedText());
+    //             await webViewController?.clearFocus();
+    //           })
+    //     ],
+    //     options: ContextMenuOptions(hideDefaultSystemContextMenuItems: false),
+    //     onCreateContextMenu: (hitTestResult) async {
+    //       // print("onCreateContextMenu");
+    //       // print(hitTestResult.extra);
+    //       // print(await webViewController?.getSelectedText());
+    //     },
+    //     onHideContextMenu: () {
+    //       print("onHideContextMenu");
+    //     },
+    //     onContextMenuActionItemClicked: (contextMenuItemClicked) async {
+    //       var id = (Platform.isAndroid)
+    //           ? contextMenuItemClicked.androidId
+    //           : contextMenuItemClicked.iosId;
+    //       print("onContextMenuActionItemClicked: " +
+    //           id.toString() +
+    //           " " +
+    //           contextMenuItemClicked.title);
+    //     });
 
     pullToRefreshController = PullToRefreshController(
       options: PullToRefreshOptions(
@@ -193,129 +144,160 @@ class _InAppWebViewScreenState extends State<InAppWebViewScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            progress < 1.0
-                ? LinearProgressIndicator(
-                    value: progress,
-                    color: Colors.amber,
-                  )
-                : Container(),
-            Container(
-              height: 300,
-              child: InAppWebView(
-                key: webViewKey,
-                // contextMenu: contextMenu,
-                initialUrlRequest:
-                    URLRequest(url: Uri.parse('https://okjx.cc/?url=$_href')),
-                // initialFile: "assets/index.html",
-                initialUserScripts: UnmodifiableListView<UserScript>([]),
-                initialOptions: options,
-                pullToRefreshController: pullToRefreshController,
-                onWebViewCreated: (controller) {
-                  webViewController = controller;
-                },
-                onLoadStart: (controller, url) {
-                  setState(() {
-                    this.url = url.toString();
-                    urlController.text = this.url;
-                  });
-                },
-                androidOnPermissionRequest:
-                    (controller, origin, resources) async {
-                  return PermissionRequestResponse(
-                      resources: resources,
-                      action: PermissionRequestResponseAction.GRANT);
-                },
-                shouldOverrideUrlLoading: (controller, navigationAction) async {
-                  var uri = navigationAction.request.url!;
+        child: ConstrainedBox(
+          constraints: BoxConstraints.expand(),
+          child: Stack(
+            //指定未定位或部分定位widget的对齐方式
+            children: <Widget>[
+              Container(
+                height: 300,
+                child: InAppWebView(
+                  key: webViewKey,
+                  // contextMenu: contextMenu,
+                  initialUrlRequest:
+                      URLRequest(url: Uri.parse('https://okjx.cc/?url=$_href')),
+                  // initialFile: "assets/index.html",
+                  initialUserScripts: UnmodifiableListView<UserScript>([]),
+                  initialOptions: options,
+                  pullToRefreshController: pullToRefreshController,
+                  onWebViewCreated: (controller) {
+                    webViewController = controller;
+                    _loading();
+                  },
+                  onLoadStart: (controller, url) {
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  androidOnPermissionRequest:
+                      (controller, origin, resources) async {
+                    return PermissionRequestResponse(
+                        resources: resources,
+                        action: PermissionRequestResponseAction.GRANT);
+                  },
+                  shouldOverrideUrlLoading:
+                      (controller, navigationAction) async {
+                    var uri = navigationAction.request.url!;
 
-                  if (![
-                    "http",
-                    "https",
-                    "file",
-                    "chrome",
-                    "data",
-                    "javascript",
-                    "about"
-                  ].contains(uri.scheme)) {
-                    if (await canLaunch(url)) {
-                      //跳转浏览器打开app
-                      // Launch the App
-                      // await launch(
-                      //   url,
-                      // );
-                      // and cancel the request
-                      return NavigationActionPolicy.CANCEL;
+                    if (![
+                      "http",
+                      "https",
+                      "file",
+                      "chrome",
+                      "data",
+                      "javascript",
+                      "about"
+                    ].contains(uri.scheme)) {
+                      if (await canLaunch(url)) {
+                        //跳转浏览器打开app
+                        // Launch the App
+                        // await launch(
+                        //   url,
+                        // );
+                        // and cancel the request
+                        return NavigationActionPolicy.CANCEL;
+                      }
                     }
-                  }
 
-                  return NavigationActionPolicy.ALLOW;
-                },
-                onLoadStop: (controller, url) async {
-                  pullToRefreshController.endRefreshing();
-                  setState(() {
-                    this.url = url.toString();
-                    urlController.text = this.url;
-                  });
-                },
-                onLoadError: (controller, url, code, message) {
-                  pullToRefreshController.endRefreshing();
-                },
-                onProgressChanged: (controller, progress) {
-                  if (progress == 100) {
+                    return NavigationActionPolicy.ALLOW;
+                  },
+                  onLoadStop: (controller, url) async {
                     pullToRefreshController.endRefreshing();
-                  }
-                  setState(() {
-                    this.progress = progress / 100;
-                    urlController.text = this.url;
-                  });
-                },
-                onUpdateVisitedHistory: (controller, url, androidIsReload) {
-                  setState(() {
-                    this.url = url.toString();
-                    urlController.text = this.url;
-                  });
-                },
-                onConsoleMessage: (controller, consoleMessage) {
-                  print(consoleMessage);
-                },
-                onEnterFullscreen: (controller) {
-                  AutoOrientation.landscapeAutoMode(forceSensor: true);
-                  print("进入");
-                },
-                onExitFullscreen: (controller) {
-                  AutoOrientation.portraitUpMode();
-                  print("退出");
-                },
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onLoadError: (controller, url, code, message) {
+                    pullToRefreshController.endRefreshing();
+                  },
+                  onProgressChanged: (controller, progress) {
+                    if (progress == 100) {
+                      pullToRefreshController.endRefreshing();
+                    }
+                    setState(() {
+                      this.progress = progress / 100;
+                      urlController.text = this.url;
+                    });
+                  },
+                  onUpdateVisitedHistory: (controller, url, androidIsReload) {
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onConsoleMessage: (controller, consoleMessage) {
+                    print(consoleMessage);
+                  },
+                  onEnterFullscreen: (controller) {
+                    AutoOrientation.landscapeAutoMode(forceSensor: true);
+                    print("进入");
+                  },
+                  onExitFullscreen: (controller) {
+                    AutoOrientation.portraitUpMode();
+                    print("退出");
+                  },
+                ),
               ),
-            ),
-            Container(
-              //要给个高度 内容Container也要给个高度 来适应屏幕高度
-              height: 30,
-              color: Colors.purple,
-              child: TabBar(
-                  isScrollable: true, controller: _tabcontroller, tabs: _tabs),
-            ),
-            Container(
-              //这个是显示内容的视图
-              //给个高度高度一定要给 不然报错
-              height: 0,
-              child:
-                  TabBarView(controller: _tabcontroller, children: _tabViews),
-            )
-          ],
+              progress < 1.0
+                  ? LinearProgressIndicator(
+                      value: progress,
+                      color: Colors.amber,
+                    )
+                  : Container(),
+              Positioned(
+                top: 300,
+                child: Container(
+                  //要给个高度 内容Container也要给个高度 来适应屏幕高度
+                  height: 40,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.purple,
+                  child: TabBar(
+                      isScrollable: true,
+                      controller: _tabcontroller,
+                      tabs: _tabs),
+                ),
+              ),
+              this._data.length > 0
+                  ? Obx(() => Positioned(
+                      top: 350,
+                      height: _data[_tabcontroller.index]!.data!.length <= 20
+                          ? (_viewsHeight.value + 100).toDouble()
+                          : (_viewsHeight.value).toDouble(),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height:
+                              _data[_tabcontroller.index]!.data!.length <= 20
+                                  ? _viewsHeight.value + 100
+                                  : _viewsHeight.value + 220,
+                          child: TabBarView(
+                              physics: NeverScrollableScrollPhysics(),
+                              controller: _tabcontroller,
+                              children: _tabViews),
+                        ),
+                      )))
+                  : Container()
+            ],
+          ),
         ),
+
+        // Column(
+
+        //   ],
+        // ),
       ),
     );
   }
 
   Future<ApiResponse<TVideoAnthologyList>> getData() async {
-    EasyLoading.show(status: 'loading...');
+    // EasyLoading.show(status: 'loading...');
     dynamic res = await HttpUtils.post('${_baseUrl}${_VideoDetails}',
         data: {"cid": '$_cid'});
     TVideoAnthologyList data = TVideoAnthologyList.fromJson(res);
-    EasyLoading.dismiss();
+    // EasyLoading.dismiss();
 
     return ApiResponse.completed(data);
   }
@@ -327,6 +309,18 @@ class _InAppWebViewScreenState extends State<InAppWebViewScreen>
         _data = tVideoDataResponse.data!.data!;
         _tabcontroller = TabController(length: _data.length, vsync: this);
       });
+
+      _viewsHeight.value =
+          (_data[_tabcontroller.index]!.data!.length / 5 * 30).toInt();
+
+      _tabcontroller
+        ..addListener(() {
+          if (_tabcontroller.index.toDouble() ==
+              _tabcontroller.animation!.value) {
+            _viewsHeight.value =
+                (_data[_tabcontroller.index]!.data!.length / 5 * 30).toInt();
+          }
+        });
       getTabs();
     }
   }
